c2d9afae3bd82fbc6eee2974bb66df3c508b8fee
Igor Redchuk <redchuk@gmail.com>
2017-06-29 00:24:45 +0200

    Add dist file

---
+++ dist/index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+function chainValidationRules(rules) {
+	if (!Array.isArray(rules) || rules.length === 0) {
+		return undefined;
+	}
+
+	if (rules.length === 1) {
+		return rules[0];
+	}
+
+	var resolveResult = function resolveResult(_ref) {
+		var result = _ref.result,
+		    val = _ref.val,
+		    param = _ref.param,
+		    prev = _ref.prev,
+		    next = _ref.next,
+		    cb = _ref.cb;
+
+		if (result === true || result && result.isValid) {
+			if (next.async) {
+				next.validator(val, param, cb);
+			} else {
+				cb({ isValid: next.validator(val, param), message: next.message });
+			}
+		} else {
+			cb({ isValid: false, message: result && result.message || prev.message });
+		}
+	};
+
+	var combineTwo = function combineTwo(prev, next) {
+		if (!prev.async && !next.async) {
+			return [prev, next];
+		}
+
+		return {
+			async: true,
+			message: next.message,
+			validator: function validator(val, param, cb) {
+				if (prev.async) {
+					prev.validator(val, param, function (result) {
+						return resolveResult({ result: result, val: val, param: param, prev: prev, next: next, cb: cb });
+					});
+				} else {
+					resolveResult({ result: prev.validator(val, param), val: val, param: param, prev: prev, next: next, cb: cb });
+				}
+			}
+		};
+	};
+
+	return rules.reduce(function (prev, next) {
+		return combineTwo(prev, next);
+	});
+}
+
+exports.default = chainValidationRules;
