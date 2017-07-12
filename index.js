aa32dc2681ef4a12bb43f2fb46136e6f5fe028ea
Igor Redchuk <redchuk@gmail.com>
2017-07-12 22:51:07 +0200

    Use standard style

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-function chainValidationRules(rules) {
-	if (!Array.isArray(rules) || rules.length === 0) {
-		return undefined;
-	}
+/* eslint-disable standard/no-callback-literal */
+function chainValidationRules (rules) {
+  if (!Array.isArray(rules) || rules.length === 0) {
+    return undefined
+  }
 
-	if (rules.length === 1) {
-		return rules[0];
-	}
+  if (rules.length === 1) {
+    return rules[0]
+  }
 
-	const resolveResult = function ({result, val, param, prev, next, cb}) {
-		if (result === true || (result && result.isValid)) {
-			if (next.async) {
-				next.validator(val, param, cb);
-			} else {
-				cb({isValid: next.validator(val, param), message: next.message});
-			}
-		} else {
-			cb({isValid: false, message: (result && result.message) || prev.message});
-		}
-	};
+  const resolveResult = function ({ result, val, param, prev, next, cb }) {
+    if (result === true || (result && result.isValid)) {
+      if (next.async) {
+        next.validator(val, param, cb)
+      } else {
+        cb({ isValid: next.validator(val, param), message: next.message })
+      }
+    } else {
+      cb({ isValid: false, message: (result && result.message) || prev.message })
+    }
+  }
 
-	const combineTwo = function (prev, next) {
-		if (!prev.async && !next.async) {
-			return [prev, next];
-		}
+  const combineTwo = function (prev, next) {
+    if (!prev.async && !next.async) {
+      return [prev, next]
+    }
 
-		return {
-			async: true,
-			message: next.message,
-			validator: (val, param, cb) => {
-				if (prev.async) {
-					prev.validator(val, param, result => resolveResult({result, val, param, prev, next, cb}));
-				} else {
-					resolveResult({result: prev.validator(val, param), val, param, prev, next, cb});
-				}
-			}
-		};
-	};
+    return {
+      async: true,
+      message: next.message,
+      validator: (val, param, cb) => {
+        if (prev.async) {
+          prev.validator(val, param, result => resolveResult({ result, val, param, prev, next, cb }))
+        } else {
+          resolveResult({ result: prev.validator(val, param), val, param, prev, next, cb })
+        }
+      }
+    }
+  }
 
-	return rules.reduce((prev, next) => combineTwo(prev, next));
+  return rules.reduce((prev, next) => combineTwo(prev, next))
 }
 
-export default chainValidationRules;
+export default chainValidationRules
--- test/index.test.js
+++ test/index.test.js
@@ -1,194 +1,195 @@
-import test from 'tape';
-import chainValidationRules from '../index';
+/* eslint-disable standard/no-callback-literal */
+import test from 'tape'
+import chainValidationRules from '../index'
 
 test('when pass undefined, returns undefined', t => {
-	t.plan(1);
-	const actual = chainValidationRules(undefined);
-	t.equal(actual, undefined);
-});
+  t.plan(1)
+  const actual = chainValidationRules(undefined)
+  t.equal(actual, undefined)
+})
 
 test('when pass an empty array, returns undefined', t => {
-	t.plan(1);
-	const actual = chainValidationRules([]);
-	t.equal(actual, undefined);
-});
+  t.plan(1)
+  const actual = chainValidationRules([])
+  t.equal(actual, undefined)
+})
 
 test('when pass an array containing one async rule, validate with this rule', t => {
-	t.plan(2);
+  t.plan(2)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb(value === 0)
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb(value === 0)
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(0, undefined, result => {
-		t.equal(result, true);
-	});
-});
+  actual.validator(0, undefined, result => {
+    t.equal(result, true)
+  })
+})
 
 test('when pass an array containing one async rule, invalidate with this rule', t => {
-	t.plan(3);
+  t.plan(3)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value === 0, message: 'Error message'})
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value === 0, message: 'Error message' })
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(1, undefined, result => {
-		t.equal(result.isValid, false);
-		t.equal(result.message, 'Error message');
-	});
-});
+  actual.validator(1, undefined, result => {
+    t.equal(result.isValid, false)
+    t.equal(result.message, 'Error message')
+  })
+})
 
 test('when pass an array containing two async rules, and both rules validate', t => {
-	t.plan(2);
+  t.plan(2)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 2 === 0, message: 'Message 1'})
-		},
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 3 === 0, message: 'Message 2'})
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 2 === 0, message: 'Message 1' })
+    },
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 3 === 0, message: 'Message 2' })
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(6, undefined, result => {
-		t.equal(result.isValid, true);
-	});
-});
+  actual.validator(6, undefined, result => {
+    t.equal(result.isValid, true)
+  })
+})
 
 test('when pass an array containing two async rules, and first rule invalidates', t => {
-	t.plan(3);
+  t.plan(3)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 2 === 0, message: 'Message 1'})
-		},
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 3 === 0, message: 'Message 2'})
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 2 === 0, message: 'Message 1' })
+    },
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 3 === 0, message: 'Message 2' })
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(3, undefined, result => {
-		t.equal(result.isValid, false);
-		t.equal(result.message, 'Message 1');
-	});
-});
+  actual.validator(3, undefined, result => {
+    t.equal(result.isValid, false)
+    t.equal(result.message, 'Message 1')
+  })
+})
 
 test('when pass an array containing two async rules, and second rule invalidates', t => {
-	t.plan(3);
+  t.plan(3)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 2 === 0, message: 'Message 1'})
-		},
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 3 === 0, message: 'Message 2'})
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 2 === 0, message: 'Message 1' })
+    },
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 3 === 0, message: 'Message 2' })
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(4, undefined, result => {
-		t.equal(result.isValid, false);
-		t.equal(result.message, 'Message 2');
-	});
-});
+  actual.validator(4, undefined, result => {
+    t.equal(result.isValid, false)
+    t.equal(result.message, 'Message 2')
+  })
+})
 
 test('when pass an array containing async and sync rules, and all rules validate', t => {
-	t.plan(2);
+  t.plan(2)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 2 === 0, message: 'Message 1'})
-		},
-		{
-			async: false,
-			message: 'Message 2',
-			validator: value => value % 3 === 0
-		},
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 5 === 0, message: 'Message 3'})
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 2 === 0, message: 'Message 1' })
+    },
+    {
+      async: false,
+      message: 'Message 2',
+      validator: value => value % 3 === 0
+    },
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 5 === 0, message: 'Message 3' })
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(30, undefined, result => {
-		t.equal(result.isValid, true);
-	});
-});
+  actual.validator(30, undefined, result => {
+    t.equal(result.isValid, true)
+  })
+})
 
 test('when pass an array containing async and sync rules, and sync rule invalidates', t => {
-	t.plan(3);
+  t.plan(3)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 2 === 0, message: 'Message 1'})
-		},
-		{
-			async: false,
-			message: 'Message 2',
-			validator: value => value % 3 === 0
-		},
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 5 === 0, message: 'Message 3'})
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 2 === 0, message: 'Message 1' })
+    },
+    {
+      async: false,
+      message: 'Message 2',
+      validator: value => value % 3 === 0
+    },
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 5 === 0, message: 'Message 3' })
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(10, undefined, result => {
-		t.equal(result.isValid, false);
-		t.equal(result.message, 'Message 2');
-	});
-});
+  actual.validator(10, undefined, result => {
+    t.equal(result.isValid, false)
+    t.equal(result.message, 'Message 2')
+  })
+})
 
 test('when pass an array containing async and sync rules, and last async rule invalidates', t => {
-	t.plan(3);
+  t.plan(3)
 
-	const actual = chainValidationRules([
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 2 === 0, message: 'Message 1'})
-		},
-		{
-			async: false,
-			message: 'Message 2',
-			validator: value => value % 3 === 0
-		},
-		{
-			async: true,
-			validator: (value, param, cb) => cb({isValid: value % 5 === 0, message: 'Message 3'})
-		}
-	]);
+  const actual = chainValidationRules([
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 2 === 0, message: 'Message 1' })
+    },
+    {
+      async: false,
+      message: 'Message 2',
+      validator: value => value % 3 === 0
+    },
+    {
+      async: true,
+      validator: (value, param, cb) => cb({ isValid: value % 5 === 0, message: 'Message 3' })
+    }
+  ])
 
-	t.equal('function', typeof actual.validator);
+  t.equal('function', typeof actual.validator)
 
-	actual.validator(6, undefined, result => {
-		t.equal(result.isValid, false);
-		t.equal(result.message, 'Message 3');
-	});
-});
+  actual.validator(6, undefined, result => {
+    t.equal(result.isValid, false)
+    t.equal(result.message, 'Message 3')
+  })
+})
